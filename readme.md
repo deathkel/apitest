0fc1bcb3a5f17af2f59fc5c29c18b883aea2eb3e
KEL <455989416@qq.com>
2016-07-22 16:46:45 +0800

    delete function belong to 2.0

--- readme.md
+++ readme.md
@@ -16,3 +16,28 @@ composer require deathkel/apitest
             'ApiTest\ApiTestServiceProvider'
         ]
 ```
+运行 'php artisan vender:publish'将视图文件和静态文件发布到你们的项目中，请确保文件夹resource/views/api和public/api文件夹为空。
+否则请自行复制使用本项目frotend文件中的blade和静态文件
+  
+##Route配置示例
+请配置在debug模式开启下的路由，不要再生产环境中使用
+
+```
+    if (config('app.debug')) {
+        Route::get('apitest','ApiTestController@index');
+    }
+```
+##控制器示例
+```
+    use ApiTest\ApiReflection
+    
+    class ApiTestController extend Controller{
+        public function index(){
+            $reflection=new ApiReflection();
+            $api=$reflection->getApi();
+            $apiToString=json_encode($api);
+            
+            return view('api.index', ['api' => $api,'apiToString'=>$apiToString]);
+        }
+    }
+```
--- src/ApiReflection.php
+++ src/ApiReflection.php
@@ -21,7 +21,7 @@ class ApiReflection
      */
     public function getApi(){
         if($this->config==null){
-           $this->config=$this->setConfigAuto();
+           $this->setConfigAuto();
         }
         $arr=array();
         foreach($this->config as $class)
@@ -49,6 +49,7 @@ class ApiReflection
      * @param class
      * @return array
      * 获取单个类的反射api
+     * y
      */
     protected function getApiByClass($class){
         $reflection=new \ReflectionClass($class);
@@ -65,6 +66,7 @@ class ApiReflection
      * 提取控制器名字
      * @param controllerName
      * @return string
+     * y
      */
     protected function shortenControllerName($name){
         preg_match('/\w*Controller\.php/',$name,$start,PREG_OFFSET_CAPTURE);//匹配 *Controller.php起始位置
@@ -82,15 +84,14 @@ class ApiReflection
     /**
      * @param $reflection
      * @return array
+     * y
      */
-    protected function getAllMethodApi($reflection)
+    protected function getAllMethodApi(\ReflectionClass $reflection)
     {
-//        require_once(config('app.library_dir')."Reflection/RouteParser.php");
-
         $api=array();
         $methods = $reflection->getMethods();
         foreach($methods as $key=>$property) {
-            //只返回子类的方法
+            //filter function like public and common
             if(!$property->isPublic())break;
             if( $property->getName()=="json")break;
 
@@ -112,167 +113,7 @@ class ApiReflection
         return $api;
     }
 
-//    /**
-//     * 获取routes.php文件 --Yuan
-//     */
-//    public function getRoutes() {
-//
-//        $routes=\Route::getRoutes();
-//        $arr = array();
-//        $pc = array();
-//        $mobile = array();
-//        $common = array();
-//        $modules = array();
-//
-//        //分类（pc、mobile、common、modules）
-//        foreach($routes as $route) {
-//            $actionName = $route->getActionName();
-//            //dump($actionName);
-//            //匹配Pc
-//            if(preg_match('/Pc/',$actionName)) {
-//                array_push($pc,$route);
-//                continue;
-//            }
-//
-//            //匹配Modules
-//            if(preg_match('/Modules/',$actionName)) {
-//                array_push($modules,$route);
-//                continue;
-//            }
-//
-//            //匹配Mobile
-//            if(preg_match('/Mobile/',$actionName)) {
-//                array_push($mobile,$route);
-//                continue;
-//            }
-//
-//            //匹配Common
-//            if(preg_match('/Common/',$actionName)) {
-//                array_push($common,$route);
-//                continue;
-//            }
-//
-//
-//
-//        }
-//        $arr['pc'] = $this->getApiData($pc);
-//        $arr['mobile'] = $this->getApiData($mobile);
-//        $arr['common'] = $this->getApiData($common);
-//        $arr['modules'] = $this->getModulesData($modules);//$this->getApiData($modules);
-//
-//        return $arr;
-//    }
-//
-//    public function getModuleRoutes($params) {
-//
-//        $arr = Array();
-//        foreach($params as $key=>$param) {
-//
-//            $admin = preg_match('/Admin/',$param['file']);
-//            $user = preg_match('/User/',$param['file']);
-//
-//            if($admin) {
-//                $str= 'Admin/'.$param['controller'];
-//                $arr[$key]['controller'] = $str;
-//            }
-//            if($user) {
-//                $str= 'User/'.$param['controller'];
-//                $arr[$key]['controller'] = $str;
-//            }
-//            $arr[$key]['methods'] = $this->getApiByFile($param['file']);
-//        }
-//
-//        return $arr;
-//    }
-//
-//    protected function getApiData($routes) {
-//
-//        $objs = $this->removeDuplicate($routes);
-//
-//        foreach($objs as $key=>$obj) {
-//            $objs[$key]['methods'] = $this->getApiByFile($obj['file']);
-//        }
-//        return $objs;
-//    }
-//
-//    //获得每个模块的controller以及文件位置
-//   protected function getModulesData($routes) {
-//       $objs = $this->removeDuplicate($routes);
-//       $arr = array();
-//       foreach($routes as $route) {
-//           $name=$this->getModulesName($route->getActionName());
-//
-//           foreach($objs as $key=>$obj) {
-//               $obj_name = $this->getModulesName($obj['file']);
-//
-//               if($obj_name == $name) {
-//                   $arr[$name][$key] = $obj;
-//               }
-//           }
-//
-//       }
-//       return $arr;
-//   }
-//
-//    protected function getModulesName($name) {
-//        preg_match('/Modules/',$name,$start,PREG_OFFSET_CAPTURE);
-//        preg_match('/Http/',$name,$end,PREG_OFFSET_CAPTURE);
-//        $string_length = strlen('modules/');
-//        $result = substr($name,$start[0][1]+$string_length,$end[0][1]-$start[0][1]-$string_length-1);
-//        return $result;
-//    }
 
-    //将重复的controller去掉 获取controller和文件路径
-    //remove repeated controller,get controller,file path
-    protected function removeDuplicate($routes) {
-        $arr = array();
-        foreach($routes as $key=>$route) {
-            $route->file = $this->shortenActionName($route);
-            $route->repeat = false;
-        }
-
-        $length = count($routes);
-        for($i=0;$i<$length;$i++) {
-            for($j=$i+1;$j<$length;$j++){
-                if($routes[$i]->file == $routes[$j]->file) {
-                    $routes[$j]->repeat = true;
-                }
-            }
-        }
-
-        foreach($routes as $key=>$route) {
-            if(!$route->repeat) {
-                $arr[$key]['file'] = $route->file;
-                $arr[$key]['controller'] = $this->getControllerName($route->getActionName());
-            }
-        }
-
-        return $arr;
-    }
-
-    protected function shortenActionName($route) {
-        $name = $route->getActionName();
-        preg_match('/App/', $name, $start, PREG_OFFSET_CAPTURE);
-        preg_match('/@/', $name, $end, PREG_OFFSET_CAPTURE);
-        $result = substr($name, $start[0][1], $end[0][1] - $start[0][1]);
-
-        return $result;
-    }
-
-    protected function getControllerName($name) {
-        preg_match('/\w*Controller\@/',$name,$start,PREG_OFFSET_CAPTURE);//匹配 *Controller.php起始位置
-        preg_match('/Controller\@/',$name,$end,PREG_OFFSET_CAPTURE);//匹配 *Controller.php结束位置
-
-        return substr($name,$start[0][1],$end[0][1]-$start[0][1]);
-    }
-
-    protected function getApiByFile($class) {
-        $reflection=new \ReflectionClass($class);
-        //require_once(config('app.library_dir') . "Reflection/DocParser.php");
-        $methods=$this->getAllMethodApi($reflection);
-
-        return $methods;
-    }
 
 
 
